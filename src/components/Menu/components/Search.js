bc8b71be00af9cb57e4c501997e70de67170638e
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-01-31 12:53:02 -0300

    style do search

--- src/components/Menu/components/Search.js
+++ src/components/Menu/components/Search.js
@@ -7,7 +7,7 @@ const StyledSearch = styled.div`
   border: 1px solid ${({ theme }) => theme.borderBase};
   max-width: 425px;
   width: 100%;
-  border-radius: 2px;
+  border-radius: 6px;
   overflow: hidden;
   
   input {
