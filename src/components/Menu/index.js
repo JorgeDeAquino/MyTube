4693ff0c23096e5321d1bb2fb1cf5c007228f6eb
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-02-01 21:13:47 -0300

    alteracoes base

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
+
 
 const StyledMenu = styled.header`
   display: flex;
@@ -32,6 +34,7 @@ export default function Menu({ valorDoFiltro, setValorDoFiltro }) {
         <Logo />
       </div>
       <Search valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
