39887d642ebb192287cd7a2e44e1c3f3471ac6f6
Jorge Alexandre <jorgeaamsantos@gmail.com>
2023-02-01 21:04:14 -0300

    alteracoes em index

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,19 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-    const estilosDaHomePage = {
-        // backgroundColor: "red" 
-    };
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
                 flex: 1,
+                // backgroundColor: "red",
             }}>
                 {/* Prop Drilling */}
                 <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
